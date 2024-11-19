d7b35e73fed9761e8243f7c238cf2b35a3360670
Ojieh94 <140613578+Ojieh94@users.noreply.github.com>
2024-11-19 01:01:51 +0100

    included the users router

--- app/main.py
+++ app/main.py
@@ -2,14 +2,15 @@ from fastapi import FastAPI
 from sqlalchemy.orm import Session
 from app.database import engine, Base
 
-from app.routers import auth, hospital
+from app.routers import auth, hospitals, users
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 app.include_router(auth.router)
-app.include_router(hospital.router)
+app.include_router(hospitals.router)
+app.include_router(users.router)
 
 
 @app.get('/')
