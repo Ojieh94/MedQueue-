3d966244ba7fd1d0a5f43ad4254d1c51d01b8fc7
prevail <esehgodprevail@gmail.com>
2024-11-27 10:59:00 +0100

    still on the cors issue

--- app/main.py
+++ app/main.py
@@ -7,21 +7,19 @@ from app.routers import auth, hospitals, users, doctors, sign_up_link as link_ge
 from app.crud import sign_up_link as link
 from fastapi.middleware.cors import CORSMiddleware
 
-origins = ["*"]
 
 app = FastAPI()
 
-# origins = [
-#     "http://localhost",
-#     "http://localhost:3000",
-#     "https://queue-medix.vercel.app"
-# ]
+origins = [
+    "http://localhost",
+    "http://localhost:3000"
+]
 
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
     # allow_origins=origins,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True, 
     allow_methods=["*"],  
     allow_headers=["*"]
