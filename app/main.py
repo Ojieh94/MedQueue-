d85a18f38353f532dbd40f5c458ca3ded71a3f49
Ojieh94 <140613578+Ojieh94@users.noreply.github.com>
2024-11-28 12:42:37 +0100

    reverted cors to original setting

--- app/main.py
+++ app/main.py
@@ -10,23 +10,13 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# origins = [
-#     "http://localhost",
-#     "http://localhost:3000",
-#     "http://localhost:3001"
-# ]
-
-# Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=origins,
-    allow_origins=["http://localhost:3000"],
-    allow_credentials=True, 
-    allow_methods=["*"],  
-    allow_headers=["*"]
-    # allow_headers=["*", "Authorization"], 
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
-
 
 
 Base.metadata.create_all(bind=engine)
