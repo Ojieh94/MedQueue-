9851290e1c2c40f1fbcd65ab82ac82b0a6d3a169
prevail <esehgodprevail@gmail.com>
2024-11-29 09:21:22 +0100

    merge error

--- app/main.py
+++ app/main.py
@@ -10,11 +10,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# origins = [
-#     "http://localhost",
-#     "http://localhost:3000",
-#     "http://localhost:3001"
-# ]
+# origins = [*]
 
 # Add CORS middleware
 app.add_middleware(
