1c1d566b372b74666593c80018326097aea7d87f
prevail <esehgodprevail@gmail.com>
2024-12-15 15:24:16 +0100

    doctor endpoints

--- app/routers/doctors.py
+++ app/routers/doctors.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from app.oauth2 import get_current_user
 from sqlalchemy.orm import Session
 from app import schemas
-from app.crud import doctors as doctor_crud, admins as admin_crud
+from app.crud import doctors as doctor_crud, admins as admin_crud, hospitals as hos_crud
 from app.database import get_db
 
 router = APIRouter(
@@ -11,9 +11,9 @@ router = APIRouter(
 )
 
 @router.get('/doctors', status_code=200, response_model=List[schemas.DoctorResponse])
-def get_all_doctors(db: Session = Depends(get_db), name: str = None, specialization: str = None, current_user: schemas.Admin = Depends(get_current_user), offset: int = 0, limit: int = 10):
-    if current_user.admin_type != schemas.AdminType.SUPER_ADMIN:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Super Admin privileges required")
+def get_all_doctors(db: Session = Depends(get_db), name: str = None, specialization: str = None,  offset: int = 0, limit: int = 10):
+    # if current_user.admin_type != schemas.AdminType.SUPER_ADMIN:
+    #     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Super Admin privileges required")
      
     doctors = doctor_crud.get_doctors(db=db, name=name, specialization=specialization, offset=offset, limit=limit)
 
@@ -21,7 +21,7 @@ def get_all_doctors(db: Session = Depends(get_db), name: str = None, specializat
 
 
 @router.get('/doctors/{doctor_id}', status_code=200, response_model=schemas.DoctorResponse)
-def get_doctor_by_id(doctor_id: int, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
+def get_doctor_by_id(doctor_id: int, db: Session = Depends(get_db)):
     # Retrieve doctor by ID
     doctor = doctor_crud.get_doctor(db=db, doctor_id=doctor_id)
     if not doctor:
@@ -29,34 +29,38 @@ def get_doctor_by_id(doctor_id: int, db: Session = Depends(get_db), current_user
             status_code=status.HTTP_404_NOT_FOUND, detail="Doctor not found")
 
     # Check if the current user is the doctor
-    current_doc = doctor_crud.get_doctor_by_user_id(
-        db=db, user_id=current_user.id)
-    if current_doc and current_doc.id != doctor_id:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail="Not authorized to perform this action")
+    # current_doc = doctor_crud.get_doctor_by_user_id(
+    #     db=db, user_id=current_user.id)
+    # if current_doc and current_doc.id != doctor_id:
+    #     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+    #                         detail="Not authorized to perform this action")
 
-    # Retrieve admin data
-    current_admin = admin_crud.get_admin_by_user_id(db=db, user_id=current_user.id)
-    if current_admin is None:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                            detail="Admin privileges required")
+    # # Retrieve admin data
+    # current_admin = admin_crud.get_admin_by_user_id(db=db, user_id=current_user.id)
+    # if current_admin is None:
+    #     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+    #                         detail="Admin privileges required")
 
-    # Check hospital authorization for non-super admin
-    if current_admin.admin_type != schemas.AdminType.SUPER_ADMIN:
-        if current_admin.hospital_id != doctor.hospital_id:
-            raise HTTPException(
-                status_code=status.HTTP_403_FORBIDDEN, detail="You cannot perform this action")
+    # # Check hospital authorization for non-super admin
+    # if current_admin.admin_type != schemas.AdminType.SUPER_ADMIN:
+    #     if current_admin.hospital_id != doctor.hospital_id:
+    #         raise HTTPException(
+    #             status_code=status.HTTP_403_FORBIDDEN, detail="You cannot perform this action")
 
     return doctor
 
 
 
 @router.get('/doctors/availability/{hospital_id}', status_code=200, response_model=List[schemas.DoctorResponse])
-def get_available_doctors(hospital_id: int, db: Session = Depends(get_db), current_user: schemas.Admin = Depends(get_current_user), offset: int = 0, limit: int = 10):
-    if current_user.admin_type != schemas.AdminType.SUPER_ADMIN:
-        if current_user.hospital_id != hospital_id:
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You cannot check doctors of other hospitals")
-        
+def get_available_doctors(hospital_id: int, db: Session = Depends(get_db), offset: int = 0, limit: int = 10):
+    # if current_user.admin_type != schemas.AdminType.SUPER_ADMIN:
+    #     if current_user.hospital_id != hospital_id:
+    #         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You cannot check doctors of other hospitals")
+    hospital = hos_crud.get_hospital_id(hospital_id, db)
+    if not hospital:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Hospital not found")
+    
     doctors = doctor_crud.get_available_doctors(db=db, hospital_id=hospital_id, offset=offset, limit=limit)
 
     return doctors
